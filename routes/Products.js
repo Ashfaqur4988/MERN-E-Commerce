48ac09cd712f8a8e4cb5bb2c169206a699fb7905
Ashfaqur Riaz <ashfakurriaz@gmail.com>
2023-11-06 16:27:05 +0530

    FINAL BUILD

--- routes/Products.js
+++ routes/Products.js
@@ -5,6 +5,7 @@ const {
   updateProduct,
 } = require("../controller/Product");
 const express = require("express");
+const { Product } = require("../model/Product");
 
 const router = express.Router();
 
@@ -15,6 +16,18 @@ router
   .get("/", fetchAllProduct) //fetching all products with filter, sort, pagination
   .get("/:id", fetchProductById) //fetch product with id (from parameter)
   .patch("/:id", updateProduct); //update product
+
+// .get("update/test", async (req, res) => {
+//   const products = await Product.find({});
+//   for (let product of products) {
+//     product.discountPrice = Math.round(
+//       product * price * (1 - product.discountPercentage / 100)
+//     );
+//     await product.save();
+//     console.log(product.title + " updated");
+//   }
+//   res.send("ok");
+//});
 
 //exporting the router
 exports.router = router;
